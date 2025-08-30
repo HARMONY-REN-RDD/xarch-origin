6a0cfe929a29230215d8c7b9739663a09207b541
kai5041 <kai5041contact@gmail.com>
2025-08-30 20:38:57 +0200

    Backup

--- x32/xwm-img/src/load_architecture.cpp
+++
@@ -1,7 +0,0 @@
-#include <xarch/origin/x32.hpp>
-#include <xwm/orchestrator.hpp>
-
-XWM_EXPORT
-xwm::BaseArchitecture *load_architecture(xwm::Orchestrator *orchestrator) {
-  return new xarch::origin::XarchOrigin_x32();
-}
